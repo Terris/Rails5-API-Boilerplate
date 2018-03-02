c72913a6bd4136b68ec22dea85c7c27714a1d07c
“Terris“ <terris.kremer@gmail.com>
2018-03-02 14:09:36 -0700

    move auth routes to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      post 'login', to: 'authentication#authenticate'
+      post 'signup', to: 'users#create'
+      #resources :users
     end
   end
-
-  post 'auth/login', to: 'authentication#authenticate'
-  post 'signup', to: 'users#create'
 end
